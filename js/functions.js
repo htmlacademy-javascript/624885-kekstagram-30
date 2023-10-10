d5a965d8a966a11d3f4193092ae8b26d0c48cfd6
ivnikonHA <ivnikon@mail.ru>
2023-10-10 16:06:01 +0300

    Исправил замечания к пулл-реквесту

--- js/functions.js
+++ js/functions.js
@@ -2,15 +2,9 @@ const checkStringLength = function (str, len) {
   return str.length <= len;
 };
 
-const checkRevesableString = function (str) {
+const checkReversableString = function (str) {
   const normalizedString = str.replaceAll(' ','').toLowerCase();
   const newString = normalizedString.split('').reverse().join('');
-
-  // let newString = '';
-  // for (let i = normalizedString.length - 1; i >= 0; i--) {
-  //   newString += normalizedString[i];
-  // }
-
   return normalizedString === newString;
 };
 
@@ -26,5 +20,5 @@ const getNumberFromString = function (str) {
 };
 
 checkStringLength('проверяемая строка', 20);
-checkRevesableString('Лёша на полке клопа нашёл ');
+checkReversableString('Лёша на полке клопа нашёл ');
 getNumberFromString('1 кефир, 0.5 батона');
